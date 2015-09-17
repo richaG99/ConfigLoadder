6a721204e901fe7f4f423e449ceadcce60ba3a33
System Administrator <root@Richas-Mac.local>
2015-09-18 01:50:45 +0530

    adding config loader project to github

---
+++ test.js
@@ -0,0 +1,3 @@
+/**
+ * Created by richa on 06/09/15.
+ */
---
+++ test/testutil.js
@@ -0,0 +1,14 @@
+
+exports.add=function(x,y)
+{
+    return x+y;
+};
+
+//var x="abc<";
+//var y= x.split("<");
+//console.log("length :"+ y.length);
+//console.log(">>>>>>>>>>>>>" + y[0]);
+//console.log(">>>>>>>>>>>>>" + y[1].length);
+//
+//var x="[pawan]";
+//console.log(">>>>>"+ x.substr(1, x.length-2))
